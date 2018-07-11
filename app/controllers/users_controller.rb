e146695aa2ee7288dfe7bf34c76eaf2851dcd788
Huyen Cherry <huyencherry137@gmail.com>
2018-07-11 08:59:40 +0700

    Chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :load_user, only: [:show, :edit, :update, :destroy] 
+
+  def index
+    @users = User.page(params[:page]).per Settings.per_page
+  end
+
   def show
-    @user = User.find_by id: params[:id]
     return if @user
     flash[:danger] = t("something")
     redirect_to root_path
@@ -21,9 +29,54 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t("page_edit")
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t("del_user")
+    else
+      flash[:danger] = t("error")
+      redirect_to users_url
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
   end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t("require_login")
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t("something")
+    redirect_to root_path
+  end
 end
+
